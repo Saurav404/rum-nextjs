3c69d533abdbc159e6c1a0ae91a473ebb03ad8c3
unknown <saurab.rawat@jamsfy.com>
2022-12-20 16:04:32 +0530

    main

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Script
-        strategy="afterInteractive"
         id="my-script"
         dangerouslySetInnerHTML={{
           __html: `window.rumv = window.rumv || function() { (window.rumv.q = window.rumv.q || []).push(arguments) };
@@ -16,7 +15,7 @@ function MyApp({ Component, pageProps }) {
               }
               
               var head = si.querySelector('head'), js = si.createElement('script');
-              js.src = 'https://cdn.jsdelivr.net/gh/Saurav404/rum-script@244a6ba/rum.js';
+              js.src = 'https://cdn.jsdelivr.net/gh/Saurav404/rum-script@151232c/rum.js';
               vi.rumv.storage = s;
               head.appendChild(js);
           })('2F7D77C9C7', window, document, location, 'rum-nextjs-project.vercel.app')`,
