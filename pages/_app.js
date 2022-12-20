38aa26083b486f9959d059fcef7069a8c5141f47
unknown <saurab.rawat@jamsfy.com>
2022-12-20 12:28:56 +0530

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -5,30 +5,18 @@ function MyApp({ Component, pageProps }) {
     <>
       <script
         dangerouslySetInnerHTML={{
-          __html: `window.rumv =
-          window.rumv ||
-          function () {
-            (window.rumv.q = window.rumv.q || []).push(arguments);
-          };
-        (function (rum, vi, si, on) {
-          var s = JSON.parse(sessionStorage.getItem("rumv") || '{"pageviews":0}');
-          s.pageviews++;
-          if (
-            s.urls &&
-            s.regex &&
-            (s.page = eval("(" + s.regex + ")")(s.urls, on.pathname)) &&
-            !s.page.type
-          ) {
-            return sessionStorage.setItem("rumv", JSON.stringify(s));
-          }
-        
-          var head = si.querySelector("head"),
-            js = si.createElement("script");
-            // use script.js file as src
-          js.src = "https://cdn.jsdelivr.net/gh/Saurav404/rum-script@6bc2606/rum.js";
-          vi.rumv.storage = s;
-          head.appendChild(js);
-        })("2F7D77C9C7", window, document, location, "rum-nextjs-project.vercel.app")`,
+          __html: `window.rumv = window.rumv || function() { (window.rumv.q = window.rumv.q || []).push(arguments) };
+          (function(rum, vi,si,on) {
+              var s = JSON.parse( sessionStorage.getItem('rumv') || '{"pageviews":0}' ); s.pageviews++;
+              if ( s.urls && s.regex && ( s.page = eval('('+s.regex+')')( s.urls, on.pathname ) ) && !s.page.type ) {
+                  return sessionStorage.setItem('rumv', JSON.stringify( s ) );
+              }
+              
+              var head = si.querySelector('head'), js = si.createElement('script');
+              js.src = 'https://cdn.jsdelivr.net/gh/Saurav404/rum-script@6bc2606/rum.js';
+              vi.rumv.storage = s;
+              head.appendChild(js);
+          })('2F7D77C9C7', window, document, location, 'rum-nextjs-project.vercel.app')`,
         }}
       />
       <Component {...pageProps} />
