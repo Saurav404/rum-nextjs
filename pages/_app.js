01155d6acb338ee7a3826381fd4c95c29b0d9644
unknown <saurab.rawat@jamsfy.com>
2022-12-21 16:08:39 +0530

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,23 @@
 import "../styles/globals.css";
-
+import Script from "next/script";
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `window.rumv = window.rumv || function() { (window.rumv.q = window.rumv.q || []).push(arguments) };
+    (function(rum, vi,si,on) {
+        var s = JSON.parse( sessionStorage.getItem('rumv') || '{"pageviews":0}' ); s.pageviews++;
+        if ( s.urls && s.regex && ( s.page = eval('('+s.regex+')')( s.urls, on.pathname ) ) && !s.page.type ) {
+            return sessionStorage.setItem('rumv', JSON.stringify( s ) );
+        } 
+        var head = si.querySelector('head'), js = si.createElement('script');
+        js.src = 'https://cdn.jsdelivr.net/gh/Saurav404/rum-script@5b3496d/rum.js';
+        vi.rumv.storage = s;
+        head.appendChild(js);
+    })('2F7D77C9C7', window, document, location, 'rum-nextjs-project.vercel.app')`,
+        }}
+      />
       <Component {...pageProps} />
     </>
   );
