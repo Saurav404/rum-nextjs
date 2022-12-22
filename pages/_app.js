b379020b0d997b47dbb45b94af9fcc86ca7e7fc7
unknown <saurab.rawat@jamsfy.com>
2022-12-22 12:30:28 +0530

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,7 @@ function MyApp({ Component, pageProps }) {
             return sessionStorage.setItem('rumv', JSON.stringify( s ) );
         } 
         var head = si.querySelector('head'), js = si.createElement('script');
-        js.src = 'https://cdn.jsdelivr.net/gh/Saurav404/rum-script@5b3496d/rum.js';
+        js.src = 'https://cdn.jsdelivr.net/gh/Saurav404/rum-script@9e63cee/rum.js';
         vi.rumv.storage = s;
         head.appendChild(js);
     })('2F7D77C9C7', window, document, location, 'rum-nextjs-project.vercel.app')`,
