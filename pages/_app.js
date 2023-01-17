67f4fff5195b89f0ff21a39665f72bcd3da4637f
unknown <saurav.rwt1102@gmail.com>
2023-01-17 15:36:36 +0530

    updated script

--- pages/_app.js
+++ pages/_app.js
@@ -6,17 +6,30 @@ function MyApp({ Component, pageProps }) {
       <Script
         id="script"
         dangerouslySetInnerHTML={{
-          __html: `window.rumv = window.rumv || function() { (window.rumv.q = window.rumv.q || []).push(arguments) };
-    (function(rum, vi,si,on) {
-        var s = JSON.parse( sessionStorage.getItem('rumv') || '{"pageviews":0}' ); s.pageviews++;
-        if ( s.urls && s.regex && ( s.page = eval('('+s.regex+')')( s.urls, on.pathname ) ) && !s.page.type ) {
-            return sessionStorage.setItem('rumv', JSON.stringify( s ) );
-        } 
-        var head = si.querySelector('head'), js = si.createElement('script');
-        js.src = 'https://cdn.jsdelivr.net/gh/Saurav404/rum-script@de56fc5/rum.js';
-        vi.rumv.storage = s;
-        head.appendChild(js);
-    })('2F7D77C9C7', window, document, location, 'rum-nextjs-project.vercel.app')`,
+          __html: `window.adz =
+          window.adz ||
+          function () {
+            (window.adz.q = window.adz.q || []).push(arguments);
+          };
+        (function (rum) {
+          (w = window), (d = document), (l = location);
+          var s = JSON.parse(sessionStorage.getItem("adz") || '{"pageviews":0}');
+          s.pageviews++;
+          if (
+            s.urls &&
+            s.regex &&
+            (s.page = eval("(" + s.regex + ")")(s.urls, l.pathname)) &&
+            !s.page.type
+          ) {
+            return sessionStorage.setItem("adz", JSON.stringify(s));
+          }
+        
+          var head = d.querySelector("head"),
+            js = d.createElement("script");
+          js.src = "https://cdn.jsdelivr.net/gh/Saurav404/rum-script@de56fc5/rum.js";
+          w.adz.storage = s;
+          head.appendChild(js);
+        })("2F7D77C9C7")`,
         }}
       />
       <Component {...pageProps} />
