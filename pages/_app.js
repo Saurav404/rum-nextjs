997b7a6c16fd4da682f56f2e8483864e4ef49e5c
unknown <saurav.rwt1102@gmail.com>
2023-02-01 17:08:16 +0530

    new script

--- pages/_app.js
+++ pages/_app.js
@@ -28,7 +28,7 @@ function MyApp({ Component, pageProps }) {
             var js = d.createElement("script");
             js.defer = true;
             js.crossOrigin = "anonymous";
-            js.src = "https://cdn.jsdelivr.net/gh/Saurav404/rum-script@429a151/rum.js";
+            js.src = "https://cdn.jsdelivr.net/gh/Saurav404/rum-script@34fb0e9/rum.js";
             d.querySelector("head").appendChild(js);
           })(window, document, sessionStorage)`,
         }}
