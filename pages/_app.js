3cdcdff8b40ab221ecf47a280f5d19498a55cf44
unknown <saurav.rwt1102@gmail.com>
2023-02-01 16:11:21 +0530

    new script

--- pages/_app.js
+++ pages/_app.js
@@ -6,30 +6,31 @@ function MyApp({ Component, pageProps }) {
       <Script
         id="script"
         dangerouslySetInnerHTML={{
-          __html: `window.adz =
-          window.adz ||
-          function () {
-            (window.adz.q = window.adz.q || []).push(arguments);
-          };
-        (function (rum) {
-          (w = window), (d = document), (l = location);
-          var s = JSON.parse(sessionStorage.getItem("adz") || '{"pageviews":0}');
-          s.pageviews++;
-          if (
-            s.urls &&
-            s.regex &&
-            (s.page = eval("(" + s.regex + ")")(s.urls, l.pathname)) &&
-            !s.page.type
-          ) {
-            return sessionStorage.setItem("adz", JSON.stringify(s));
-          }
-        
-          var head = d.querySelector("head"),
-            js = d.createElement("script");
-          js.src = "https://cdn.jsdelivr.net/gh/Saurav404/rum-script@cc98f23/rum.js";
-          w.adz.storage = s;
-          head.appendChild(js);
-        })("2F7D77C9C7")`,
+          __html: `!(function (w, d, s) {
+            w.adz =
+              w.adz ||
+              function () {
+                (w.adz.q = w.adz.q || []).push(arguments);
+              };
+            var l = location;
+            var ss = JSON.parse(s.getItem("adz") || '{"pageviews":0}');
+            ss.pageviews++;
+            if (
+              ss.urls &&
+              ss.regex &&
+              (ss.page = eval("(" + ss.regex + ")")(ss.urls, l.pathname)) &&
+              !ss.page.type
+            ) {
+              return s.setItem("adz", JSON.stringify(ss));
+            }
+          
+            w.adz.storage = ss;
+            var js = d.createElement("script");
+            js.defer = true;
+            js.crossOrigin = "anonymous";
+            js.src = "https://cdn.jsdelivr.net/gh/Saurav404/rum-script@6f87271/rum.js";
+            d.querySelector("head").appendChild(js);
+          })(window, document, sessionStorage)`,
         }}
       />
       <Component {...pageProps} />
