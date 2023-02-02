a402e7b07745114deaa95703a8c75ac1b8b79034
unknown <saurav.rwt1102@gmail.com>
2023-02-02 13:39:55 +0530

    change

--- pages/_app.js
+++ pages/_app.js
@@ -28,7 +28,7 @@ function MyApp({ Component, pageProps }) {
             var js = d.createElement("script");
             js.defer = true;
             js.crossOrigin = "anonymous";
-            js.src = "https://cdn.jsdelivr.net/gh/Saurav404/rum-script@7317192/rum.js";
+            js.src = "https://cdn.jsdelivr.net/gh/Saurav404/rum-script@277c241/rum.js";
             d.querySelector("head").appendChild(js);
           })(window, document, sessionStorage)`,
         }}
