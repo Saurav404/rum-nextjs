0717ba2d02adfd932ad4d530ee168cf830dbc600
unknown <saurav.rwt1102@gmail.com>
2023-03-02 12:22:53 +0530

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -7,10 +7,12 @@ function MyApp({ Component, pageProps }) {
         id="script"
         dangerouslySetInnerHTML={{
           __html: `window.adz = window.adz || function() { (window.adz.q = window.adz.q || []).push(arguments) };
+          debugger;
           (function(rum, vi,si,on, d) {
               if ( !new RegExp([d].join('|')).test( on.hostname ) ) {
                   return;
               }
+              debugger;
               var s = JSON.parse( sessionStorage.getItem('adz') || '{"pageviews":0}' ); s.pageviews++;
               if ( s.urls && s.regex && ( s.page = eval('('+s.regex+')')( s.urls, on.pathname ) ) && !s.page.type ) {
                   return sessionStorage.setItem('adz', JSON.stringify( s ) );
@@ -18,7 +20,7 @@ function MyApp({ Component, pageProps }) {
               
               vi.adz.storage = s;
               var head = si.querySelector('head'), js = si.createElement('script');
-              js.src = "https://cdn.jsdelivr.net/gh/Saurav404/rum-script@5eba80b/rum.js";
+              js.src = "https://cdn.jsdelivr.net/gh/Saurav404/rum-script@7412987/rum.js";
               head.appendChild(js);
           })( '9C5E5F4B26', window, document, location, 'rum-nextjs-project.vercel.app' );`,
         }}
